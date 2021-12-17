6a7942445627db00fb0cd777d44d189741ad5c7e
pputyoursoulin <93555090+pputyoursoulin@users.noreply.github.com>
2021-12-17 09:09:12 +0300

    Add files via upload

--- Z5.py
+++ Z5.py
@@ -1,14 +1,53 @@
 file = open('data.txt')
 
-try:
-    massiv = list(map(int, file.read().split()))
 
-    if len(massiv) < 1:
+
+try:
+    chislo = list(map(int, file.readline().split()))
+
+    if (len(chislo) < 2) or (chislo[0] <= 0):
          print('Error!!! The data in the file is incorrect!')
     else:
-         value = massiv[0]
-         if massiv.count(value) == len(massiv): print('The sequence consists of identical numbers\n-----')
-         else: print('The sequence consists of different numbers\n-----')
+        count = 0
+        for i in range(len(chislo)):
+            if (chislo[i] < 0) or (chislo[i] > 9):
+                count += 1
+        if count == 0:
+            print('Start number: ')
+            for i in range(len(chislo)):
+                print(chislo[i],end = '')
+            print('\n')
+            a = int(input('Enter the operation:\n1 - "+1"\n2 - "-1"\n\n'))
+            if a == 1:
+                chislo[-1] += 1
+                for i in range(len(chislo) -1, -1, -1):
+                    if chislo[i] == 10:
+                        if i == 0:
+                            chislo[i] = 0
+                            chislo.insert(0, 1)
+                        else:
+                            chislo[i] = 0
+                            chislo[i-1] += 1
+                for i in range(len(chislo)):
+                    print(chislo[i],end = '')
+            else:
+                if a == 2:
+                    chislo[-1] -= 1
+                    for i in range(len(chislo) -1, -1, -1):
+                        if chislo[i] == -1:
+                            chislo[i] = 9
+                            chislo[i-1] -= 1
+                    if chislo[0] == 0:
+                        chislo.pop(0)
+                    for i in range(len(chislo)):
+                        print(chislo[i],end = '')
+                else:
+                    print('Operation entered incorrectly')
+        else:
+            print('Error!!! The data in the file is incorrect!')
+   
+
+    print('\n')
 except ValueError:
     print('Error!!! The data in the file is incorrect!')
 
